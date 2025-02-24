f786df2049935b8f4a3a4039769cae3149984257
shivam kumar <rajput.shivam75298@gmail.com>
2025-02-24 23:17:53 +0530

    basic edits

--- self-runner/src/components/EC2.jsx
+++ self-runner/src/components/EC2.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
 import "../styles/styles.css"; // Corrected the CSS file path
 
 function EC2() {
